0f04e9a79f657ebe67b77276a3e6eedb1d847dce
LDF <47423029+LucaDFill@users.noreply.github.com>
2021-04-05 11:55:31 +0200

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.7.0;
 
+// interface to be imported in an external file.
+// only interface methods are callable from another smart contract
 interface IHelloWorld {
     function sayHelloWorld() external pure returns(string memory);
 }
 
+// HelloWorld implements IHelloWorld, overriding it's function
 contract HelloWorld is IHelloWorld{
     
     function sayHelloWorld() external pure override returns(string memory) {
